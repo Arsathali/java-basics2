6d8bb6216e2177a3306905a0335a29a4a48b18de
arsath ali <arsatharsha17@gmail.com>
2026-01-28 10:35:33 +0530

    Add While Lopp Problems

---
+++ src/com/problems/whileLoop/SumOfWhile.java
@@ -0,0 +1,16 @@
+package com.problems.whileLoop;
+
+public class SumOfWhile{
+    
+    public static void main(String[] args) {
+        
+        int sum =0;
+        int i = 1;
+        int n = 5;
+        while(i<=n){
+            sum+=i;
+        }
+
+        System.out.println(sum);
+    }
+}
